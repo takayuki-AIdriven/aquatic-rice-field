27f8882b460b67c7aa1b775cb5566b2636289ad7
takayuki-Aldriven <sti2750@gmail.com>
2026-06-25 14:33:35 +0900

    fix: force Vercel to rebuild by updating a source file

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,7 @@ import { FarmWorkspace } from "@/components/workspace/FarmWorkspace";
 import { FARM_WORKSPACE_DATA } from "@/lib/farm-data";
 
 export default function Page() {
+  // Force Vercel to trigger a new build by changing a source file
   return <FarmWorkspace data={FARM_WORKSPACE_DATA} />;
 }
+
